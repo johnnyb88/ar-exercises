376bfa96f16725cb46665977a0e943f3a6e4812f
johnnyb88 <johnnybarratt@gmail.com>
2019-11-12 18:03:32 +0000

    update the first store

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -5,3 +5,8 @@ puts "Exercise 2"
 puts "----------"
 
 # Your code goes here ...
+# Once an Active Record object has been retrieved, its attributes can be modified and it can be saved to the database.
+pp @store1 = Store.find_by(id: 1)
+pp @store2 = Store.find_by(id: 2)
+
+pp @store1.name = "Kitsilano"
